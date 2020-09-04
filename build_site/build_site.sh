e3dcdb60043cac634187a69ad82cb5ba96ab082f
b3m2a1 <b3m2a1@gmail.com>
2020-09-04 16:22:31 -0400

    Update build_site.sh

--- build_site/build_site.sh
+++ build_site/build_site.sh
@@ -5,18 +5,18 @@ set -e
 ## Builds out the website
 ##
 
-echo $(ls)
-
-## configure git
-#git config --global user.name '${GITHUB_ACTOR}'
-#git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'
-## clone in the group repos
-#git clone https://github.com/McCoyGroup/Peeves.git
-#git clone https://github.com/McCoyGroup/McUtils.git
-#git clone https://github.com/McCoyGroup/Psience.git
-#git clone https://github.com/McCoyGroup/RynLib.git
-## run the docs script
-#python3 References/docs.py
-### push back to main
-#git add -A && git commit -m "Built out site"
-#git push -u origin master HEAD
+# get out of the References folder
+cd ..
+# configure git
+git config --global user.name '${GITHUB_ACTOR}'
+git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'
+# clone in the group repos
+git clone https://github.com/McCoyGroup/Peeves.git
+git clone https://github.com/McCoyGroup/McUtils.git
+git clone https://github.com/McCoyGroup/Psience.git
+git clone https://github.com/McCoyGroup/RynLib.git
+# run the docs script
+python3 References/docs.py
+## push back to main
+git add -A && git commit -m "Built out site"
+git push -u origin master HEAD
