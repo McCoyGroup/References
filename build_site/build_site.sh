db2f15a3d266d0bd4e965459da2cf6498e7bd025
b3m2a1 <b3m2a1@gmail.com>
2020-09-04 16:51:55 -0400

    Location issues...

--- build_site/build_site.sh
+++ build_site/build_site.sh
@@ -6,17 +6,19 @@ set -e
 ##
 
 # get into the parent folder?
-cd /home/runner/work/References
+echo $(ls /)
+echo $(ls /home)
+cd /home/runner/work/
 # configure git
-git config --global user.name '${GITHUB_ACTOR}'
-git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'
-# clone in the group repos
-git clone https://github.com/McCoyGroup/Peeves.git
-git clone https://github.com/McCoyGroup/McUtils.git
-git clone https://github.com/McCoyGroup/Psience.git
-git clone https://github.com/McCoyGroup/RynLib.git
-# run the docs script
-python3 References/docs.py
-## push back to main
-git add -A && git commit -m "Built out site"
-git push -u origin master HEAD
+#git config --global user.name '${GITHUB_ACTOR}'
+#git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'
+## clone in the group repos
+#git clone https://github.com/McCoyGroup/Peeves.git
+#git clone https://github.com/McCoyGroup/McUtils.git
+#git clone https://github.com/McCoyGroup/Psience.git
+#git clone https://github.com/McCoyGroup/RynLib.git
+## run the docs script
+#python3 References/docs.py
+### push back to main
+#git add -A && git commit -m "Built out site"
+#git push -u origin master HEAD
