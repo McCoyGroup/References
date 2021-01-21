e0a7fa29abdfc9244abf0284d875539a316ba644
b3m2a1 <b3m2a1@gmail.com>
2021-01-21 02:05:04 -0500

    Update build_site.sh

--- build_site/build_site.sh
+++ build_site/build_site.sh
@@ -18,6 +18,14 @@ git clone https://github.com/McCoyGroup/RynLib.git
 ## make sure we're build off of an up to date repo
 cd References
 git pull
+
+# Zip and push exercises changes
+cd McCoy\ Group\ Code\ Academy
+zip -r Exercises.zip Exercises
+cd ..
+git add -A && git commit -m "updated exercises"
+git push -u origin HEAD
+
 ## run the docs script
 cd /home
 PYTHONPATH=/home python3 References/docs.py
