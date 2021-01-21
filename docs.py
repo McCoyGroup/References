39de2e88a0beb33169e90ee84918acc3ba7bd30c
b3m2a1 <b3m2a1@gmail.com>
2021-01-21 02:35:47 -0500

    Update docs.py

--- docs.py
+++ docs.py
@@ -1,16 +1,30 @@
 from Peeves.Doc import *
-import os
+import os, sys
 
-__pkgs__ = [
-    "McUtils",
-    "Psience",
-    "Peeves",
-    "RynLib"
-]
-
-target = os.path.join(os.path.dirname(__file__), "Documentation")
-DocWalker(
-    __pkgs__,
-    out=target,
-    ignore_paths=['RynLib.md']
-).write_docs()
+root = os.path.dirname(os.path.dirname(__file__))
+target = os.path.join(root, "Documentation")
+sys.path.insert(0, root)
+doc_config = {
+    "packages": [
+        {
+            "id": "McUtils",
+            'tests_root': os.path.join(os.path.dirname(root), "McUtils", "ci", "tests")
+        },
+        {
+            "id": "Psience",
+            'tests_root': os.path.join(os.path.dirname(root), "Psience", "ci", "tests")
+        },
+        {
+            "id": "Peeves",
+            'tests_root': os.path.join(os.path.dirname(root), "Peeves", "ci", "tests")
+        },
+        {
+            "id": "RynLib",
+            'tests_root': os.path.join(os.path.dirname(root), "RynLib", "ci", "tests")
+        }
+    ],
+    "root": root,
+    "target": target,
+    "readme": os.path.join(root, "README.md")
+}
+DocBuilder(**doc_config).build()
